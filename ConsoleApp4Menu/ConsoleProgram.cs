6cb06eddadb24a553c38adf935d368c75cb1ba98
Adam Hansen <adamlars90@gmail.com>
2017-08-14 12:36:47 +0200

    Added Menu Assignment

---
+++ ConsoleApp4Menu/ConsoleProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4Menu
+{
+    static class ConsoleProgram
+    {
+        static void Main(string[] args)
+        {
+            var menuItems = new[]
+            {
+                "List All Customers",
+                "Add Customer",
+                "Delete Customer",
+                "Edit Customer",
+                "Exit"
+            };
+
+            Console.WriteLine("Select what you want to do:");
+            //foreach (var menuItem in menuItems)
+            //{
+            //    Console.WriteLine(menuItem);
+            //}
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}: {menuItems[i]}");
+            }
+
+            var userInput = Console.ReadLine();
+        }
+    }
+}
